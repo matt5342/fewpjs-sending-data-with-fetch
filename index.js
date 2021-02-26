b0258d9db1686966485df50adbd9cc37f12faf81
Matt <mattfsewell@gmail.com>
2021-02-26 14:01:39 -0500

    commit tree 73335cbbd38b1cdae8ba66934f9bce29c37c8b31

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    let newUser = {
+        name: name, 
+        email: email
+    }
+    let reqObj = {
+        headers: {"Content-Type": 'application/json', "Accept": "application/json"}, 
+        method: "POST", 
+        body: JSON.stringify(newUser)
+    }
+    fetch('http://localhost:3000/users', reqObj)
+        .then(r => r.json())
+        .then(json => addId(json.id))
+}
+
+function addId(id) {
+    document.getElementsByTagName("body").innerText = id
+}
+submitData("bill", "b@b.com")
